fd173be17c37ab56c70d3a7b2f5f5e2381a2d71d
GEGZ <gegzgegu@outlook.com>
2026-03-11 22:21:00 +0100

    commit

---
+++ pointcloud/cloud.js
@@ -0,0 +1,30 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": " +a=0 +b=0 +units=m",
+    "points": 3907305,
+    "boundingBox": {
+        "lx": -46.684100,
+        "ly": -109.298200,
+        "lz": -11.879200,
+        "ux": 217.049726,
+        "uy": 154.435626,
+        "uz": 251.854626
+    },
+    "tightBoundingBox": {
+        "lx": -35.514100,
+        "ly": -97.988200,
+        "lz": -9.469200,
+        "ux": 76.119449,
+        "uy": 15.047031,
+        "uz": 14.575846
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 2.060421,
+    "scale": 0.010000,
+    "hierarchyStepSize": 5
+}
